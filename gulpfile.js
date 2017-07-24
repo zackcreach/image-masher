3153ec346cf0fc389f4c021eed782c1bf8ffba9c
zackcreach <zackcreach@gmail.com>
2017-07-24 13:13:43 -0400

    Its alive!

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var imagemin = require('gulp-imagemin');
 var path = require('path');
 
 gulp.task('compress', function() {
-  gulp.src('/Users/zack/Desktop/import')
+  gulp.src('/Users/zack/Desktop/import/**/*')
     .pipe(imagemin([
       imagemin.gifsicle({interlaced: true}),
       imagemin.jpegtran({progressive: true}),
       imagemin.optipng({optimizationLevel: 5}),
       imagemin.svgo({plugins: [{removeViewBox: true}]})
-    ],
+    ],{
       verbose: true
-    ))
+    }))
     .pipe(gulp.dest('/Users/zack/Desktop/export'));
 });
