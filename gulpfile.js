ff0e31aeb2aae1e74958b1c9f904b31ae3b09b56
zackcreach <zackcreach@gmail.com>
2017-07-24 13:05:52 -0400

    Initial gulp task complete, ready for testing.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var path = require('path');
+
+gulp.task('compress', function() {
+  gulp.src('/Users/zack/Desktop/import')
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({plugins: [{removeViewBox: true}]})
+    ],
+      verbose: true
+    ))
+    .pipe(gulp.dest('/Users/zack/Desktop/export'));
+});
